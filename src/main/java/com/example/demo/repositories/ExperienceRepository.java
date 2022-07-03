1340cfb467577862b55dd521d2ca53b16828e473
nicolas.delgado <nicolas.delgado@birchmangroup.com>
2022-07-03 15:15:47 -0300

    comment

--- src/main/java/com/example/demo/repositories/ExperienceRepository.java
+++ src/main/java/com/example/demo/repositories/ExperienceRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repositories;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
